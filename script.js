23f27b750439e65759f470a09f41fabb5c77311a
Jojordanme <135418765+Jojordanme@users.noreply.github.com>
2024-08-13 11:59:47 +0000

    no prefcteot

--- script.js
+++ script.js
@@ -83,13 +83,12 @@ function lightsOUT(){
     document.getElementById('lightBlub').classList.remove("lightBulb")
       document.getElementById('lightBlub').classList.add("lightBulbNormal")
     document.getElementById('lightBlub').src = "OFF.png";
-  },1499)
+  },1500)
   setTimeout(function(){
     
     
-    setTimeout(function(){
-      document.body.style.backgroundColor = "black";
-    },345)
+    document.body.style.backgroundColor = "black";
+
     setTimeout(function(){
       document.getElementById('lightBlub').classList.remove("lightBulbNormal")
       document.getElementById('lightBlub').classList.add("lightBulb2")
